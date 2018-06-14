5be636cbdb4e2e95a066aa1aa62f75cdcebcd0a7
Marek Woś <marek.wos@getresponse.com>
2018-06-14 13:26:37 +0200

    Wywalenie bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['all' => true],
+    SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle::class => ['all' => true]
 ];
